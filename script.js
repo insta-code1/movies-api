318e7ffff919b66f8db4cc193c19aa7dbb937f73
insta-code1 <eoinkane1@gmail.com>
2017-01-12 11:36:36 +0000

    Connected to OMDB Api

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+var posterElement = document.querySelector('#poster');
+var plotElement = document.querySelector('#plot');
+
 var http = new XMLHttpRequest();
 var url = 'http://www.omdbapi.com/?t=lord+of+the+rings&y=&plot=short&r=json';
 var method = 'GET';
 
+http.open(method, url);
+http.onreadystatechange = function() {
+  if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+    console.log(JSON.parse(http.responseText, null, 4).Poster);
+    var data = JSON.parse(http.responseText, null, 4);
+
+    posterElement.src = data.Poster;
+    plotElement.innerHTML = data.Plot;
+
+
+
+  } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+    console.log('Error');
+  }
+};
+http.send();
