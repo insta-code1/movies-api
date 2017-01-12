655ac5eb3114d972eaaf3ee0e1b16eb7963f4c16
insta-code1 <eoinkane1@gmail.com>
2017-01-12 16:48:16 +0000

    JS targeting input feild

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ var http = new XMLHttpRequest();
 var url = 'http://www.omdbapi.com/?t=lord+of+the+rings&y=&plot=short&r=json';
 var method = 'GET';
 
+
+document.getElementById('submit-btn').addEventListener("click", function(event){
+  event.preventDefault();
+});
+
+
+function fetchMovie() {
+  alert(document.getElementById('movie-value')['value']);
+}
+
+
+
+
+
 http.open(method, url);
 http.onreadystatechange = function() {
   if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
