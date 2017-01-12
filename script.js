f59583c25f00696376f0591b94a7fa13b582386d
insta-code1 <eoinkane1@gmail.com>
2017-01-12 17:08:17 +0000

    rendering data on screen

--- script.js
+++ script.js
@@ -2,36 +2,37 @@ var posterElement = document.querySelector('#poster');
 var plotElement = document.querySelector('#plot');
 
 var http = new XMLHttpRequest();
-var url = 'http://www.omdbapi.com/?t=lord+of+the+rings&y=&plot=short&r=json';
 var method = 'GET';
 
 
-document.getElementById('submit-btn').addEventListener("click", function(event){
+document.getElementById('submit-btn').addEventListener("click", function (event) {
   event.preventDefault();
 });
 
-
 function fetchMovie() {
-  alert(document.getElementById('movie-value')['value']);
+  var movieRef = document.getElementById('movie-value')['value'];
+  return concatUrl(encodeURI(movieRef));
+}
+
+function concatUrl(title) {
+  var url = 'http://www.omdbapi.com/?t=' + title + '&y=&plot=short&r=json';
+  return fetchData(url);
+}
+
+function fetchData(url) {
+  http.open(this.method, url);
+  http.onreadystatechange = function () {
+    if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+      console.log(JSON.parse(http.responseText, null, 4).Poster);
+      var data = JSON.parse(http.responseText, null, 4);
+      posterElement.src = data.Poster;
+      plotElement.innerHTML = data.Plot;
+    } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+      console.log('Error');
+    }
+  };
+  http.send();
 }
 
 
 
-
-
-http.open(method, url);
-http.onreadystatechange = function() {
-  if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-    console.log(JSON.parse(http.responseText, null, 4).Poster);
-    var data = JSON.parse(http.responseText, null, 4);
-
-    posterElement.src = data.Poster;
-    plotElement.innerHTML = data.Plot;
-
-
-
-  } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
-    console.log('Error');
-  }
-};
-http.send();
